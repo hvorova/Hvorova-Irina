10ce0824496b6d8cb47eaf834ec697700f52dfc2
hvorova.ioIKB64 <36606568+hvorova@users.noreply.github.com>
2018-03-06 22:14:18 +0300

    Delete bank.py

--- bank.py
+++
@@ -1,9 +0,0 @@
-import account
-def main():
-    rate = int(input('Vvedite stavky:'))
-    money = int(input('Vvedite summy:'))
-    period = int(input('Vvedite period:'))
-    result = rt.calculate_income(rate,period,money)
-    print ("Parametri scheta:\n", "Summ",money, "\n", "Stavki", rate, "\n", "Period", period, "\n", "Obsh sum", result, "\n")
-if __name__=="__main__":
-    main()
